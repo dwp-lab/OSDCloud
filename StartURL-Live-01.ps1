382c1e58c6ac809ecef9401e2014c93fbbc975c4
dwp-lab <173300136+dwp-lab@users.noreply.github.com>
2024-06-20 09:27:02 +0200

    Add files via upload

--- StartURL-Live-01.ps1
+++ StartURL-Live-01.ps1
@@ -23,14 +23,14 @@ $result = Invoke-RestMethod -Uri $uri -Method POST -Body $body -ContentType "app
 
 if ($result) {
 
-    Invoke-WebRequest -Uri "https://github.com/dwp-lab/OSDCloud/raw/main/PCPKsp.dll" -OutFile "X:\Windows\System32\PCPKsp.dll"
+    Invoke-WebRequest -Uri "https://github.com/dwp-lab/OSDCloud/raw/main/PCPKsp.dll" -OutFile X:\Windows\System32\PCPKsp.dll
     rundll32 X:\Windows\System32\PCPKsp.dll,DllInstall
 
-    Invoke-WebRequest -Uri "https://github.com/dwp-lab/OSDCloud/raw/main/OA3.cfg"
-    Remove-Item OA3.xml -ErrorAction:SilentlyContinue
+    Invoke-WebRequest -Uri "https://github.com/dwp-lab/OSDCloud/raw/main/OA3.cfg" -OutFile OA3.cfg
+    Invoke-WebRequest -Uri "https://github.com/dwp-lab/OSDCloud/raw/main/oa3tool.exe" -OutFile oa3tool.exe
+    Remove-Item .\OA3.xml -ErrorAction:SilentlyContinue
     oa3tool.exe /Report /ConfigFile=.\OA3.cfg /NoKeyCheck
 
-    
     [xml]$xmlhash = Get-Content -Path .\OA3.xml
     $hash=$xmlhash.Key.HardwareHash
 
