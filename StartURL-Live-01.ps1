b56a73bdb5d9a31bea401675063682ed88a281cb
dwp-lab <173300136+dwp-lab@users.noreply.github.com>
2025-09-24 13:15:05 +0200

    update: switch to 24H2

--- StartURL-Live-01.ps1
+++ StartURL-Live-01.ps1
@@ -69,9 +69,10 @@ if ($result.Response -eq 0) {
     Import-Module OSD -Force
 
     Write-Host -BackgroundColor Black -ForegroundColor Green "Start OSDCloud"
-    Start-OSDCloud -ZTI -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Enterprise -OSLanguage en-us -OSLicense Retail
+    Start-OSDCloud -ZTI -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSLanguage en-us -OSLicense Retail
 
     Write-Host -BackgroundColor Black -ForegroundColor Green "Restart in 20 seconds"
     Start-Sleep -Seconds 20
     wpeutil reboot
+
 }
