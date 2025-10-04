1d3242cece85d30043143677a03dca5172422470
dwp-lab <173300136+dwp-lab@users.noreply.github.com>
2025-10-04 12:39:18 +0200

    Update StartURL-Live-01.ps1

--- StartURL-Live-01.ps1
+++ StartURL-Live-01.ps1
@@ -71,8 +71,15 @@ if ($result.Response -eq 0) {
     Write-Host -BackgroundColor Black -ForegroundColor Green "Start OSDCloud"
     Start-OSDCloud -ZTI -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSLanguage en-us -OSLicense Retail
 
+    Write-Host -BackgroundColor Black -ForegroundColor Green "Stage SetupComplete"
+    New-Item -ItemType Directory -Force -Path "C:\Windows\Setup\Scripts" | Out-Null
+
+    Invoke-WebRequest -Uri "https://github.com/dwp-lab/OSDCloud/raw/main/SetupComplete.cmd" -OutFile C:\Windows\Setup\Scripts\SetupComplete.cmd
+    Invoke-WebRequest -Uri "https://github.com/dwp-lab/OSDCloud/raw/main/Install-LCU.ps1" -OutFile C:\Windows\Setup\Scripts\Install-LCU.ps1
+
     Write-Host -BackgroundColor Black -ForegroundColor Green "Restart in 20 seconds"
     Start-Sleep -Seconds 20
     wpeutil reboot
 
 }
+
