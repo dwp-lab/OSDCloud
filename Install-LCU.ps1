2f75e0966e9b47ddb8631f808a6c765ea51b4063
dwp-lab <173300136+dwp-lab@users.noreply.github.com>
2025-10-04 13:11:29 +0200

    Update Install-LCU.ps1

--- Install-LCU.ps1
+++ Install-LCU.ps1
@@ -1,12 +1,12 @@
-$Log = "$env:SystemRoot\Temp\Install-LCU.log"
-Start-Transcript -Path $Log -Append | Out-Null
+Set-ExecutionPolicy RemoteSigned -Force -Scope CurrentUser
+
+Start-Transcript -Path "$env:SystemRoot\OSDCloud\Logs\Install-LCU.log" -Append | Out-Null
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Install-PackageProvider -Name NuGet -Force -Scope AllUsers | Out-Null
 Install-Module PSWindowsUpdate -Force -Scope AllUsers | Out-Null
-Add-WUServiceManager -MicrosoftUpdate -Confirm:$false | Out-Null
 
 # Pull latest SSU/LCU + critical updates and reboot automatically if needed
 # (Retries a couple times in case network is slow at first boot)
