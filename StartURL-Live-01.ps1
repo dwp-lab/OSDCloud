cae8f262abbee1a3b9292cade36a46cbe6a1396a
dwp-lab <173300136+dwp-lab@users.noreply.github.com>
2026-01-26 11:48:05 +0100

    Refactor StartURL-Live-01.ps1 for clarity and updates
    Adding function Invoke-ParseDate as part of troubleshooting driver download issues like in INC3065131

--- StartURL-Live-01.ps1
+++ StartURL-Live-01.ps1
@@ -68,6 +68,14 @@ if ($result.Response -eq 0) {
     Write-Host -BackgroundColor Black -ForegroundColor Green "Import OSD PowerShell Module"
     Import-Module OSD -Force
 
+    function Invoke-ParseDate {
+    param (
+        [String] $DateString
+    )
+    $Array = $DateString.Split("/")
+    Get-Date -Year $Array[2] -Month $Array[0] -Day $Array[1]
+	}
+
     Write-Host -BackgroundColor Black -ForegroundColor Green "Start OSDCloud"
     Start-OSDCloud -ZTI -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSLanguage en-us -OSLicense Retail
 
@@ -88,6 +96,7 @@ if ($result.Response -eq 0) {
     [System.Windows.MessageBox]::Show($infoMessage, 'OSDCloud', 'OK', 'Error') | Out-Null
     wpeutil reboot
 }
+
 
 
 
